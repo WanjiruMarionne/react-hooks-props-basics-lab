ec55998ec7fd82cb34daa6df800b3b522d84c9df
Wanjiru Mburu <wanjirumarionne@gmail.com>
2024-02-01 01:30:59 +0300

    Completed Props Basics Lab

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,19 @@
 import React from "react";
+import Links from "./Links";
 
-function About() {
+function About({bio, githubLink, linkedinLink}) {
+  let bioContent = null;
+
+  if (bio && bio.trim() !== "") {
+    bioContent = <p>{bio}</p>;
+  }
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>Put the bio in here</p>
+      {bioContent}
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
       {/* add your <Links /> component here */}
+      <Links githubLink={githubLink} linkedinLink={linkedinLink} />
     </div>
   );
 }
